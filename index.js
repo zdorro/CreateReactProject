abcebd9cfad93924ccc67d96309870f5ee8dfd76
AlekseyLu <luzyanin-ae@yandex.ru>
2022-02-17 12:06:09 +0300

    Update index.js
    Задублировал строку

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ import './index.css';
         );
       }      
 
-
+      function Square(props) {
+        return (
+          <button className="square" onClick={props.onClick}>
+            {props.value}
+          </button>
+        );
+      }  
   
   class Board extends React.Component {
       constructor(props) {
