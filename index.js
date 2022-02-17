59578d8b0ca52f5313985cdc94439e633858c646
zdorro <90772559+zdorro@users.noreply.github.com>
2022-02-17 12:04:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ import './index.css';
         );
       }      
 
-  function Square(props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }    
+
   
   class Board extends React.Component {
       constructor(props) {
